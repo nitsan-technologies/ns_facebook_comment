a303f40d0f61a08629febf601538d0d953e9e534
NITSAN <sanjay@nitsan.in>
2019-12-03 09:32:50 +0530

    [TODO] Adding Gitlab-CI & Travis with PHP, TypoScript, YAML lints [TODO] Updating documentation [TODO] Added demo link [TODO] Releasing pro-version of the extenion at https://t3terminal.com/

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ns_facebook_comment
-https://extensions.typo3.org/extension/ns_facebook_comment/
+
+- FREE version: You can download FREE version with basic-features at https://extensions.typo3.org/extension/ns_guestbook/
+- PRO version: You can download PRO version with more-features & free-support at https://t3terminal.com/typo3-facebook-comment-free/
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,8 +1,8 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => '[NITSAN] Facebook Comment',
-	'description' => 'This Extension help you to add and share comment as post with Facebook',
+	'title' => '[NITSAN] Facebook Comment Plugin',
+	'description' => 'Grab this extension which will help you to add facebook comment plugin to add and share comments, reply, listing etc. Demo: https://demo.t3terminal.com/t3t-extensions/facebook-comments/ You can download PRO version for more-features & free-support at https://t3terminal.com/typo3-facebook-comment-free/',
 	'category' => 'plugin',
 	'author' => 'T3:Milan Rathod, T3:Keval Pandya, QA:Nisha Dave',
 	'author_email' => 'sanjay@nitsan.in',
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '1.0.1',
+	'version' => '1.1.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '6.0.0-9.5.99',
