0a60b4de7086332919c399494659299c375fa9eb
Mahesh Rathod <rmahesh.nitsan@gmail.com>
2021-08-05 17:44:07 +0530

    [TODO] Code improvements

--- ext_tables.php
+++ ext_tables.php
@@ -7,5 +7,5 @@ if (!defined('TYPO3_MODE')) {
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_NsFacebookComment_domain_model_NsFacebookComment');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ns_facebook_comment/Configuration/TSconfig/ContentElementWizard.txt">'
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ns_facebook_comment/Configuration/TSconfig/ContentElementWizard.tsconfig">'
 );
