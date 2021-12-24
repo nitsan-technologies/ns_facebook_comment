1db003fed7211e9fb7d0758ef09aadb5e852916c
Maulik Lakhnotra <nitsan-gitlab@fischer-and-friends.de>
2021-12-24 18:31:16 +0530

    [TODO] compatiblity with version 11

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,15 +1,20 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
+if (version_compare(TYPO3_branch, '10.0', '>=')) {
+    $moduleClass = \Nitsan\NsFacebookComment\Controller\NsFacebookCommentController::class;
+} else {
+    $moduleClass = 'NsFacebookComment';
+}
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
     'Nitsan.ns_facebook_comment',
     'NsFacebookComment',
     [
-        'NsFacebookComment' => 'list',
+        $moduleClass => 'list',
     ],
     // non-cacheable actions
     [
-        'NsFacebookComment' => 'list',
+        $moduleClass => 'list',
     ]
 );
 
