245aefe1750202bfb9524b236e4ad3053db4520c
NITSAN <sanjay@nitsan.in>
2024-05-02 12:24:57 +0530

    Update sys_template.php

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -4,4 +4,4 @@ defined('TYPO3_MODE') || die('Access denied.');
 $extKey = 'ns_facebook_comment';
 
 // Adding fields to the tt_content table definition in TCA
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript', 'Nitsan Facebook Comment');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript', 'Facebook Comment');
