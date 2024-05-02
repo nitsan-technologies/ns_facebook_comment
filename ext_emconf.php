d8d15fa78c7cd983e8531f319133af602ce05922
Harsora Krupa <harsorakrupa.nitsan@gmail.com>
2024-05-02 11:07:49 +0530

    Change title and icon

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,12 +1,12 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => '[NITSAN] Facebook Comment Plugin',
-    'description' => 'Grab this extension which will help you to add facebook comment plugin to add and share comments, reply, listing etc. Demo: https://demo.t3terminal.com/t3t-extensions/facebook-comments/ You can download PRO version for more-features & free-support at https://t3terminal.com/typo3-facebook-comment-free/',
+    'title' => 'Facebook Comment Plugin',
+    'description' => 'Grab this extension which will help you to add facebook comment plugin to add and share comments, reply, listing etc.',
     'category' => 'plugin',
-    'author' => 'NITSAN Technologies Pvt Ltd',
+    'author' => 'T3Planet // NITSAN',
     'author_email' => 'sanjay@nitsan.in',
-    'author_company' => 'NITSAN Technologies Pvt Ltd',
+    'author_company' => 'T3Planet // NITSAN',
     'state' => 'stable',
     'internal' => '',
     'uploadfolder' => '0',
